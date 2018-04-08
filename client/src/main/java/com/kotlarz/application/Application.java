dfe51456b9b75759ccbcc6704181dd020567d5e2
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-04-08 18:19:30 +0200

    Init android project

--- client/src/main/java/com/kotlarz/application/Application.java
+++ client/src/main/java/com/kotlarz/application/Application.java
@@ -2,9 +2,12 @@ package com.kotlarz.application;
 
 import com.kotlarz.service.dto.TemperatureLog;
 import com.kotlarz.service.sender.LogsSender;
+import com.kotlarz.service.sensor.MockedTemperatureReader;
+import com.kotlarz.service.sensor.RaspberryTemperatureReader;
 import com.kotlarz.service.sensor.TemperatureReader;
+import com.pi4j.system.SystemInfo;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class Application {
     public static void start() {
         LogsSender sender = new LogsSender();
-        TemperatureReader reader = null;
+        TemperatureReader reader = isRunningOnPi() ? new RaspberryTemperatureReader() : new MockedTemperatureReader();
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         executor.scheduleAtFixedRate(() -> cycle(sender, reader),
@@ -20,13 +23,25 @@ public class Application {
     }
 
     private static void cycle(LogsSender sender, TemperatureReader reader) {
-        // TODO
-
         try {
-            System.out.println("Reading mocked temperature");
-            sender.invoke(Arrays.asList(TemperatureLog.mock()));
+            System.out.println("Reading temperature");
+            List<TemperatureLog> logs = reader.readAll();
+            if (!logs.isEmpty()) {
+                sender.invoke(logs);
+            } else {
+                System.out.println("No temperature logs found");
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private static boolean isRunningOnPi() {
+        try {
+            SystemInfo.BoardType boardType = SystemInfo.getBoardType();
+            return boardType.name().toLowerCase().startsWith("raspberry");
+        } catch (Exception ex) {
+            return false;
         }
     }
 }
---
+++ client/src/main/java/com/kotlarz/service/sensor/RaspberryTemperatureReader.java
@@ -0,0 +1,38 @@
+package com.kotlarz.service.sensor;
+
+import com.kotlarz.service.dto.TemperatureLog;
+import com.pi4j.component.temperature.TemperatureSensor;
+import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
+import com.pi4j.io.w1.W1Device;
+import com.pi4j.io.w1.W1Master;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RaspberryTemperatureReader implements TemperatureReader {
+    private W1Master master;
+
+    public RaspberryTemperatureReader() {
+        master = new W1Master();
+    }
+
+    public List<TemperatureLog> readAll() {
+        synchronized (master) {
+            return master.getDevices(TmpDS18B20DeviceType.FAMILY_CODE).stream()
+                    .map(device -> (TemperatureSensor) device)
+                    .map(sensor -> TemperatureLog.builder()
+                            .address(((W1Device) sensor).getId())
+                            .date(new Date())
+                            .value(sensor.getTemperature())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public void refreshDevices() {
+        synchronized (master) {
+            master.checkDeviceChanges();
+        }
+    }
+}
